019f5adea48237c5b315d4cc720c5c803b7ca5d8
isher2k1 <ikekches@gmail.com>
2025-10-21 14:57:14 +0300

    Fixed issues in solution one more time

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -5,7 +5,7 @@ import mate.academy.service.AuthenticationService;
 public class Main {
     private static final AuthenticationService authenticationService = new AuthenticationService();
 
-    static String str = new String("hello".toCharArray());
+    private static String str = new String("hello".toCharArray());
 
     public static void main(String[] args) {
         test("bob@i.ua", "1234", true);
